d4805336cc48275137b994707dd89e566a0dd5cb
sphingomyelin-104 <shiduki_24s1300108@nnn.ed.jp>
2024-10-20 23:44:13 +0900

    デザインにブートストラッ適用

--- assessment.js
+++ assessment.js
@@ -15,14 +15,28 @@ assessmentButton.addEventListener(
         
         // 診断結果表示エリアの作成
         resultDivision.innerText = '';
-        const heading = document.createElement('h3');
-        heading.innerText = '診断結果';
-        resultDivision.appendChild(heading);
+        
+        // headerDivision の作成
+        const headerDivision = document.createElement('div');
+        headerDivision.setAttribute('class', 'card-header text-bg-primary');
+        headerDivision.innerText = '診断結果';
+
+        // bodyDivision の作成
+        const bodyDivision = document.createElement('div');
+        bodyDivision.setAttribute('class', 'card-body');
 
         const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'card-text');
         const result = assessment(userName);
         paragraph.innerText = result;
-        resultDivision.appendChild(paragraph);
+        bodyDivision.appendChild(paragraph);
+
+        // resultDivision に Bootstrap のスタイルを適用する
+        resultDivision.setAttribute('class', 'Card')
+        
+        // headerDivision と bodyDivision　を　resultDivision に差し込む
+        resultDivision.appendChild(headerDivision);
+        resultDivision.appendChild(bodyDivision);
 
         // ツイートエリアの作成
         tweetDivision.innerText = '';
@@ -92,3 +106,61 @@ function assessment(userName){
     result = result.replaceAll('###userName###', userName);
     return result;
 }
+
+// テストを行う関数
+function test() {
+    console.log('診断結果の文章のテスト');
+
+    //太郎
+    console.log('太郎');
+    console.assert(
+        assessment('太郎') ===
+        '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+        '診断結果の文言の特定の部分を名前に書き換える処理が正しくありません。'
+    );
+
+    //次郎
+    console.log('次郎');
+    console.assert(
+        assessment('次郎') ===
+        '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
+        '診断結果の文言の特定の部分を名前に書き換える処理が正しくありません。'
+    );
+
+    //花子
+    console.log('花子');
+    console.assert(
+        assessment('花子') ===
+        '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
+        '診断結果の文言の特定の部分を名前に書き換える処理が正しくありません。'
+    );
+    
+    console.log('診断結果の文章のテスト終了');
+
+    console.log('同じ名前なら同じ結果を出力することのテスト');
+
+    //太郎
+    console.log('太郎');
+    console.assert(
+        assessment('太郎') === assessment('太郎'),
+        '入力が同じなら同じ診断結果を出力する処理が正しくありません。'
+    );
+
+    //次郎
+    console.log('次郎');
+    console.assert(
+        assessment('次郎') === assessment('次郎'),
+        '入力が同じなら同じ診断結果を出力する処理が正しくありません。'
+    );
+
+    //花子
+    console.log('花子');
+    console.assert(
+        assessment('花子') === assessment('花子'),
+        '入力が同じなら同じ診断結果を出力する処理が正しくありません。'
+    );
+
+    console.log('同じ名前なら同じ結果を出力することのテスト終了');
+}
+
+test();
